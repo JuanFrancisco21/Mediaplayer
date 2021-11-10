c541e764fc3c8f30ad18abb4fd7ca3602a36d8eb
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2021-11-10 16:42:02 +0100

    Modelo de conexión

---
+++ Mediplayer/src/main/java/Model/Connection.java
@@ -0,0 +1,90 @@
+package Model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="conns")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Connection{
+    
+    private String server;
+    private String database;
+    private String userName;
+    private String password;
+    
+    public Connection(String name){
+
+        this.server="";
+        this.database="";
+        this.userName="";
+        this.password="";
+    }
+
+    public Connection(){
+        this("");
+    }
+  
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Connection{" + ", server=" + server +", database=" + database + ", userName=" + userName + ", password=" + password + '}';
+    }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((database == null) ? 0 : database.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Connection other = (Connection) obj;
+		if (database == null) {
+			if (other.database != null)
+				return false;
+		} else if (!database.equals(other.database))
+			return false;
+		return true;
+	}
+
+}
